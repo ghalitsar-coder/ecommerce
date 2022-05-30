221e9d19d40d248b56292ffab5597742d1dc544c
ghalitsar coder <ghalitsardev@gmail.com>
2022-05-30 18:33:14 +0700

    update ecommerce

---
+++ pages/cancel.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useRouter } from "next/router";
+const Cancel = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 400);
+  }, []);
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <h2>Redirected to Home!</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Cancel;
